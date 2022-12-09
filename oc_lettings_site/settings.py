2d7427cc5359e39bdb6d51d1ec4d37b084dde3c0
naegel23 <elcabiri.najim@gmail.com>
2022-12-10 00:05:48 +0100

    settings fix

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 
 sentry_sdk.init(
