fb5632423e2ed00ebb3bfeaccc53e96143220af9
naegel23 <elcabiri.najim@gmail.com>
2022-12-10 01:17:39 +0100

    settings fix

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,7 +3,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 import django_heroku
 import dj_database_url
-from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,9 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR, 'static'),)
-                    ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
